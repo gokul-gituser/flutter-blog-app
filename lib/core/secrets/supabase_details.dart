89960157335c5e38b6bb2598f39830d6396e9b83
gokul <gokuljayanjb99@outlook.com>
2024-04-30 11:10:20 +0530

    first commit

---
+++ lib/core/secrets/supabase_details.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const supabaseUrl = 'https://hxayuvvfbpyhotvyqero.supabase.co';
+  static const supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh4YXl1dnZmYnB5aG90dnlxZXJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA5MjIwMjIsImV4cCI6MjAyNjQ5ODAyMn0.Y2-GQfW1Xagy2AKHI1XJl-3Nxx39PXYoq5rEYLazvcU';
+}
